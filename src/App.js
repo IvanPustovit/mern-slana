89a1c512388d302fd8798fe2e0cdd9c44355bfe1
IvanPustovit <pustovit.for@gmail.com>
2020-09-28 23:53:44 +0300

    v3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import routesisAuth from "./routes";
+import { useAuth } from "./hooks/auth.hook";
+import { AuthContext } from "./context/AuthContext";
+import { useItem } from "./hooks/item.hook";
+
+import "materialize-css";
 
 function App() {
+  const { token, login, logout, userId, userName } = useAuth();
+  const { totalOrder, count, listCart, form, formUpdate } = useItem();
+  const isAuth = !!token;
+  const countCart = count;
+  const formItem = form;
+  const route = routesisAuth(isAuth);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider
+      value={{
+        token,
+        login,
+        logout,
+        userId,
+        isAuth,
+        userName,
+        countCart,
+        totalOrder,
+        listCart,
+        formItem,
+        formUpdate,
+      }}
+    >
+      <BrowserRouter>
+        <div className="container">{route}</div>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
